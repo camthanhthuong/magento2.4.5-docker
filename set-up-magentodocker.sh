e7a54820de1c488259bb4dee7d5522194c8fad31
camthanhthuong <84617616+camthanhthuong@users.noreply.github.com>
2022-12-13 10:48:35 +0700

    Update set-up-magentodocker.sh

--- set-up-magentodocker.sh
+++ set-up-magentodocker.sh
@@ -7,7 +7,9 @@ echo ‘The directory path is’ $DIR_PATH
 chown -R www-data:www-data $DIR_PATH/mycode
 serviceName="docker"
 if systemctl --all --type service | grep -q "$serviceName";then
-    echo "$serviceName exists." && chmod -R 777 elasticsearch-log/ && docker compose up -d
+    echo "$serviceName exists."
+    chmod -R 777 elasticsearch-log/
+    docker compose up -d
     sleep 20
     echo "sleep 20s"
     docker exec php-fpm sh install-magento2.sh
@@ -20,20 +22,28 @@ else
     curl \
     gnupg \
     lsb-release
-    sleep 1
+    sleep 3
     mkdir -p /etc/apt/keyrings
-    sleep 1s
+    sleep 3
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sleep 1
+    sleep 3
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sleep 1
+    sleep 3
     apt-get update
-    sleep 1
+    sleep 3
     chmod a+r /etc/apt/keyrings/docker.gpg
-    sleep 1
+    sleep 3
     apt-get update
-    sleep 1
-    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sleep 3
+    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    sleep 10
+    chmod -R 777 elasticsearch-log/
+    docker compose up -d
+    sleep 20
+    echo "sleep 20s"
+    docker exec php-fpm sh install-magento2.sh
+    chown -R www-data:www-data $DIR_PATH/mycode/magento2
+    
 fi
