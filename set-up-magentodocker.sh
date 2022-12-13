9713919311ab3824375e1238c481c5cb8db4b645
camthanhthuong <84617616+camthanhthuong@users.noreply.github.com>
2022-12-13 11:31:31 +0700

    Update set-up-magentodocker.sh

--- set-up-magentodocker.sh
+++ set-up-magentodocker.sh
@@ -8,7 +8,16 @@ chown -R www-data:www-data $DIR_PATH/mycode
 serviceName="docker"
 if systemctl --all --type service | grep -q "$serviceName";then
     echo "$serviceName exists."
+    mkdir -p elasticsearch-log/elastic
+    mkdir -p elasticsearch-log/logs
+    mkdir -p elasticsearch-log/elastic
+    mkdir nginx-log
+    mkdir php8.1-fpm-log
+    mkdir data-mysqldb
     chmod -R 777 elasticsearch-log/
+    chmod -R 777 nginx-log
+    chmod -R 777 php8.1-fpm-log
+    chmod -R 777 data-mysqldb
     docker compose up -d
     sleep 20
     echo "sleep 20s"
@@ -22,28 +31,35 @@ else
     curl \
     gnupg \
     lsb-release
-    sleep 3
+    sleep 1
     mkdir -p /etc/apt/keyrings
-    sleep 3
+    sleep 1s
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sleep 3
+    sleep 1
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sleep 3
+    sleep 1
     apt-get update
-    sleep 3
+    sleep 1
     chmod a+r /etc/apt/keyrings/docker.gpg
-    sleep 3
+    sleep 1
     apt-get update
-    sleep 3
-    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-    sleep 10
+    sleep 1
+    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    echo 'sleep 20s'
+    sleep 20
+    mkdir -p elasticsearch-log/elastic
+    mkdir -p elasticsearch-log/logs
+    mkdir -p elasticsearch-log/elastic
+    mkdir nginx-log
+    mkdir php8.1-fpm-log
+    mkdir data-mysqldb
     chmod -R 777 elasticsearch-log/
+    chmod -R 777 nginx-log
+    chmod -R 777 php8.1-fpm-log
+    chmod -R 777 data-mysqldb
     docker compose up -d
-    echo "sleep 50s"
-    sleep 50
     docker exec php-fpm sh install-magento2.sh
     chown -R www-data:www-data $DIR_PATH/mycode/magento2
-    
 fi
