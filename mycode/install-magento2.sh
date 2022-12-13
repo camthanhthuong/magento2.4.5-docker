c2ab50b1093b9ffd8e847cba887a02c4335f8260
camthanhthuong <84617616+camthanhthuong@users.noreply.github.com>
2022-12-13 11:06:28 +0700

    Update install-magento2.sh

--- mycode/install-magento2.sh
+++ mycode/install-magento2.sh
@@ -2,7 +2,7 @@
 
 cd /var/www/html/magento2
 sleep 2
-composer install && bin/magento setup:install --base-url=http://magento.local:8080/ \
+composer install && composer update && bin/magento setup:install --base-url=http://magento.local:8080/ \
 --db-host=127.0.0.1 --db-name=magento2 --db-user=magento2 --db-password=magento2 \
 --admin-firstname=Magento --admin-lastname=User --admin-email=user@example.com \
 --admin-user=admin --admin-password=admin123 --language=en_US \
